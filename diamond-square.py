0982c43985b8218b2642e42941c2e5d0b44ac21c
Tom <tom@fourzerofour.pw>
2020-04-13 01:16:33 +1000

    Algorithmic fixes

--- diamond-square.py
+++ diamond-square.py
@@ -102,93 +102,87 @@ def get_edge_coordinates_from_corners(corner_coordinates: Dict[Edge, Tuple[int, 
 
 def diamond_square(
     graph: SquareNodeGraph,
-    corner_coordinates: Dict[Edge, Tuple[int, int]],
+    corners: Dict[Edge, Tuple[int, int]],
     noise_scaling_factor: float = 1.0,
     noise_scaling_function: Callable = lambda f: f * 0.5
 ):
 
-    # Find edge midpoints
-    edges = get_edge_coordinates_from_corners(corner_coordinates)
-
-    # Set edge values, storing them for later (when the midpoint needs to be set)
-    edge_values: List[float] = []
-    for edge, edge_coords in edges.items():
-
-        # Gather relevant corner values
-        relevant_corner_values: List[float] = []
-        for corner, corner_coords in corner_coordinates.items():
-
-            # Recursion tracking: if the edge _is_ a corner, exit the function
-            # because clearly we've gone too deep
-            is_edge_corner_collision: bool = edge_coords == corner_coords
-            if is_edge_corner_collision:
-                return
-
-            is_relevant_corner: bool = (edge & corner) != 0
-            if is_relevant_corner:
-                corner_value = graph.get_node(corner_coords[0], corner_coords[1])
-                relevant_corner_values.append(corner_value)
-
-        # Calculate the average corner value
-        mean_corner_value = sum(relevant_corner_values) / len(relevant_corner_values) 
-
-        # Add some noise, scaled arbitrarily
-        edge_value = mean_corner_value + random.uniform(-1, 1) * noise_scaling_factor
-        edge_value = float(clamp(0, edge_value, 1))
-
-        if graph.get_node(edge_coords[0], edge_coords[1]) == 0:
-            edge_values.append(edge_value)
-        else:
-            edge_values.append(graph.get_node(edge_coords[0], edge_coords[1]))
-
-        # Set the edge value on the graph
-        graph.set_node(edge_coords[0], edge_coords[1], edge_value)
-
-    # Find midpoint coordinates
-    midpoint_coords = get_midpoint_from_tuples(
-        corner_coordinates[Edge.Top | Edge.Left],
-        corner_coordinates[Edge.Bottom | Edge.Right],
+    # Diamond step
+    midpoint = get_midpoint_from_tuples(
+        corners[Edge.Top | Edge.Left],
+        corners[Edge.Bottom | Edge.Right],
     )
 
-    # Set midpoint value to the mean of its edges, plus some noise
-    mean_edge_value = sum(edge_values) / len(edge_values)
-    midpoint_value = mean_edge_value + random.uniform(-1, 1) * noise_scaling_factor
+    corner_values = [graph.get_node(c[0], c[1]) for c in corners.values()]
+    mean_corner_value = sum(corner_values) / len(corner_values)
+    midpoint_value = mean_corner_value + random.uniform(-1, 1) * noise_scaling_factor
     midpoint_value = float(clamp(0, midpoint_value, 1))
-    graph.set_node(midpoint_coords[0], midpoint_coords[1], midpoint_value)
+
+    graph.set_node(
+        midpoint[0],
+        midpoint[1],
+        midpoint_value
+    )
+
+    # Square step
+    edges = get_edge_coordinates_from_corners(corners)
+    for edge, edge_coords in edges.items():
+
+        x, y = edge_coords
+
+        # Skip node if already set (e.g.: from an adjacent quad)
+        if graph.get_node(x, y) != 0:
+            continue
+
+        adjacent_values: List[float] = [ midpoint_value ]
+        for index, corner in enumerate(corners.keys()):
+            is_adjacent_corner: bool = (edge & corner) != 0
+            if is_adjacent_corner:
+                adjacent_values.append(corner_values[index])
+
+        mean_adjacent_value = sum(adjacent_values) / len(adjacent_values) 
+        edge_value = mean_adjacent_value + random.uniform(-1, 1) * noise_scaling_factor
+        edge_value = float(clamp(0, edge_value, 1))
+        graph.set_node(x, y, edge_value)
+
+    # Recursion break: do not recurse if we're at the most granular scale possible
+    side_length = corners[Edge.Top | Edge.Right][0] - corners[Edge.Top | Edge.Left][0]
+    if side_length <= 1:
+        return
 
     # Define sub-quads for recursion in terms of their corner vertices
-    corner_subsets = [
+    subquads = [
         {
-            Edge.Top | Edge.Left: corner_coordinates[Edge.Top | Edge.Left],
+            Edge.Top | Edge.Left: corners[Edge.Top | Edge.Left],
             Edge.Top | Edge.Right: edges[Edge.Top],
             Edge.Bottom | Edge.Left: edges[Edge.Left],
-            Edge.Bottom | Edge.Right: midpoint_coords,
+            Edge.Bottom | Edge.Right: midpoint,
         },
         {
             Edge.Top | Edge.Left: edges[Edge.Top],
-            Edge.Top | Edge.Right: corner_coordinates[Edge.Top | Edge.Right],
-            Edge.Bottom | Edge.Left: midpoint_coords,
+            Edge.Top | Edge.Right: corners[Edge.Top | Edge.Right],
+            Edge.Bottom | Edge.Left: midpoint,
             Edge.Bottom | Edge.Right: edges[Edge.Right],
         },
         {
             Edge.Top | Edge.Left: edges[Edge.Left],
-            Edge.Top | Edge.Right: midpoint_coords,
-            Edge.Bottom | Edge.Left: corner_coordinates[Edge.Bottom | Edge.Left],
+            Edge.Top | Edge.Right: midpoint,
+            Edge.Bottom | Edge.Left: corners[Edge.Bottom | Edge.Left],
             Edge.Bottom | Edge.Right: edges[Edge.Bottom],
         },
         {
-            Edge.Top | Edge.Left: midpoint_coords,
+            Edge.Top | Edge.Left: midpoint,
             Edge.Top | Edge.Right: edges[Edge.Right],
             Edge.Bottom | Edge.Left: edges[Edge.Bottom],
-            Edge.Bottom | Edge.Right: corner_coordinates[Edge.Bottom | Edge.Right],
+            Edge.Bottom | Edge.Right: corners[Edge.Bottom | Edge.Right],
         },
     ]
 
     # Apply the diamond-square algorithm recursively to those sub-quads
-    for subset in corner_subsets:
+    for subquad in subquads:
         diamond_square(
             graph,
-            subset,
+            subquad,
             noise_scaling_function(noise_scaling_factor),
             noise_scaling_function
         )
@@ -318,7 +312,7 @@ def run():
 
         diamond_square(
             graph=graph,
-            corner_coordinates={
+            corners={
                 Edge.Top | Edge.Left: [0, 0],
                 Edge.Top | Edge.Right: [size-1, 0],
                 Edge.Bottom | Edge.Left: [0, size-1],
